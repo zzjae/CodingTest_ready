e4f6ce72c0785d44cb10e7e62ff71ef6b62fa37a
GRAM <jgseo200par@naver,com>
2024-12-30 13:22:34 +0900

    ✨ Complete
    complete

--- strange_string/Main.java
+++ strange_string/Main.java
@@ -1,1 +1,24 @@
-public class Main { public static void main(String[] args) { System.out.println("Intersection Star"); } } 
+class Solution {
+  public String solution(String s) {
+
+    StringBuilder builder = new StringBuilder();
+    boolean toUpper = true;
+
+    for (char c : s.toCharArray()) {
+      if (!Character.isAlphabetic(c)) { // 공백
+        builder.append(c);
+        toUpper = true;
+      } else {
+        if (toUpper) {
+          builder.append(Character.toUpperCase(c));
+        } else {
+          builder.append(Character.toLowerCase(c));
+        }
+
+        toUpper = !toUpper;
+      }
+
+    }
+    return builder.toString();
+  }
+}
