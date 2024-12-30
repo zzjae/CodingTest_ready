9b1e900cf8741c8d49f4b6c48dc64569fd80111e
GRAM <jgseo200par@naver,com>
2024-12-30 15:51:36 +0900

    ✨ Complete
    complete

--- contain_char/Main.java
+++ contain_char/Main.java
@@ -1,1 +1,10 @@
-public class Main { public static void main(String[] args) { System.out.println("Intersection Star"); } } 
+class Solution {
+  boolean solution(String s) {
+    s = s.toLowerCase(); // 모두 소문자로 변환
+    int ps = s.length() - s.replace("p", "").length();
+    int ys = s.length() - s.replace("y", "").length();
+
+    boolean answer = ps == ys;
+    return answer;
+  }
+}
