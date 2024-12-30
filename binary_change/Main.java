2b10b056239fb0ae64a98083c18a0dd89bda868a
GRAM <jgseo200par@naver,com>
2024-12-30 14:58:30 +0900

    ✨ Complete
    complete

--- binary_change/Main.java
+++ binary_change/Main.java
@@ -1,1 +1,9 @@
-public class Main { public static void main(String[] args) { System.out.println("Intersection Star"); } } 
+class Solution {
+  public int solution(int n) {
+
+    String str = Integer.toString(n, 3); // 3진법으로 변환
+    String reversed = new StringBuilder(str).reverse().toString();
+    int answer = Integer.valueOf(reversed, 3);
+    return answer;
+  }
+}
